7460d97eed21214c7c11194e452a0b8899e9dc0e
Neves888 <helen.vneves@gmail.com>
2023-03-06 18:24:35 -0300

    confere se o teste funciona

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,20 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    produto = Product(
+        id=1546,
+        nome_da_empresa='elemento-x',
+        nome_do_produto='tetraborato de sódio',
+        data_de_fabricacao='07/07/2017',
+        data_de_validade='07/07/2037',
+        numero_de_serie='5489BD25',
+        instrucoes_de_armazenamento='manter em local seco',
+    )
+    assert produto.id == 1546
+    assert produto.nome_da_empresa == 'elemento-x'
+    assert produto.nome_do_produto == 'tetraborato de sódio'
+    assert produto.data_de_fabricacao == '07/07/2017'
+    assert produto.data_de_validade == '07/07/2037'
+    assert produto.numero_de_serie == '5489BD25'
+    assert produto.instrucoes_de_armazenamento == 'manter em local seco'
