cf7a5251fb3448d3efe1d9eeb2c596ffa30841ac
Neves888 <helen.vneves@gmail.com>
2023-03-12 12:15:09 -0300

    add ref

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -12,7 +12,7 @@ class SimpleReport:
         for item in dict_list:
             x_company.append(item["nome_da_empresa"])
             date_create.append(item["data_de_fabricacao"])
-            validate = datetime.strptime(item["data_de_validade"], '%Y-%m-%d')
+            validate = datetime.strptime(item["data_de_validade"], "%Y-%m-%d")
             if validate >= datetime.today():
                 validate_product.append((item["data_de_validade"]))
         x_company_more_products = Counter(x_company).most_common(1)
@@ -20,3 +20,6 @@ class SimpleReport:
             f"Data de fabricação mais antiga: { (min(date_create)) }\n"
             f"Data de validade mais próxima: {(min( validate_product))}\n"
             f"Empresa com mais produtos: {x_company_more_products[0][0]}")
+# resolvido com a ajuda da mentoria da Carol Weather Report
+# disponivel em: https://app.betrybe.com/learn/course/
+# 5e938f69-6e32-43b3-9685-c936530fd326/live-lectures/62d6cebd-7e2a-4e20-ba1a-54f515407308/recording/e2d95f10-7ea6-492e-bd7a-29bb138c2a5c
