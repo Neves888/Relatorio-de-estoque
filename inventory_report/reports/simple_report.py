768e6c4c1b531952c7bbfbd3ec9536f9b6b9c8ca
Neves888 <helen.vneves@gmail.com>
2023-03-12 12:12:25 -0300

    add req 2, relatorio simples

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,22 @@
+from datetime import datetime
+from collections import Counter
+
+
 class SimpleReport:
-    pass
+    @staticmethod
+    def generate(dict_list):
+        date_create = []
+        validate_product = []
+        x_company = []
+
+        for item in dict_list:
+            x_company.append(item["nome_da_empresa"])
+            date_create.append(item["data_de_fabricacao"])
+            validate = datetime.strptime(item["data_de_validade"], '%Y-%m-%d')
+            if validate >= datetime.today():
+                validate_product.append((item["data_de_validade"]))
+        x_company_more_products = Counter(x_company).most_common(1)
+        return (
+            f"Data de fabricação mais antiga: { (min(date_create)) }\n"
+            f"Data de validade mais próxima: {(min( validate_product))}\n"
+            f"Empresa com mais produtos: {x_company_more_products[0][0]}")
