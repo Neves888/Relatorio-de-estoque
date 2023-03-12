4ca3511e01610cf544ca2223b4b05664e7bb5fea
Neves888 <helen.vneves@gmail.com>
2023-03-12 12:36:21 -0300

    testa req 3

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,21 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+from collections import Counter
+
+
+class CompleteReport(SimpleReport):
+    @staticmethod
+    def generate(dict_list):
+        simple_report = SimpleReport.generate(dict_list)
+        x_company = []
+        for product in dict_list:
+            x_company.append(product["nome_da_empresa"])
+
+        x_company_counter = Counter(x_company)
+        many_company = ''
+        for item in x_company_counter:
+            many_company += f"- {item}: {x_company_counter[item]}\n"
+
+        return (
+            f"{simple_report}\n"
+            f"{many_company}"
+        )
